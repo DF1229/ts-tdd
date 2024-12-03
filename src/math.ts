5a3e466d00b9f453b94844ead8f898c00288c25c
DF1229 <daanfaber1@gmail.com>
2024-12-03 23:30:30 +0100

    Initial commit

---
+++ src/math.ts
@@ -0,0 +1,11 @@
+export function add(number1: number, number2: number): number {
+    return number1 + number2;
+}
+
+export function divide(number1: number, number2: number): number {
+    return number1 / number2;
+}
+
+export function mult(number1: number, number2: number): number {
+    return number1 * number2;
+}
---
+++ src/test/math.test.ts
@@ -0,0 +1,21 @@
+import * as math from '../math';
+
+describe('The math module', () => {
+    it('should correctly add two numbers', () => {
+        expect(math.add(1, 2)).toEqual(3);
+        expect(math.add(2, 3)).toEqual(5);
+        expect(math.add(-3, 4)).toEqual(1);
+        expect(math.add(3, -4)).toEqual(-1);
+        expect(math.add(math.add(1, 2), 5)).toEqual(8);
+    });
+
+    it('should correctly divide two numbers', () => {
+        expect(math.divide(1, 2)).toEqual(0.5);
+        expect(math.divide(2, 1)).toEqual(2);
+        expect(math.divide(5, 2)).toEqual(2.5);        
+    });
+
+    it('should multiply correctly', () => {
+        expect(math.mult(1, 3)).toEqual(3);
+    });
+});
